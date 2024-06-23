87c8b9865dd5c1caec11e98916af1e044f2a2f85
michaelangelosdr <michaelangelosdr@gmail.com>
2024-06-24 03:30:56 +0800

    Update SuperScript
    yes

--- SuperScript.cs
+++ SuperScript.cs
@@ -7,7 +7,7 @@ public class SuperScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("TEEEST");
     }
 
     // Update is called once per frame
